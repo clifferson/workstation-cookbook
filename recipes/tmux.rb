573cf57bcc79d60bd2050ac6b96e1c8688194d94
Cliff Erson <cerson@me.com>
2013-06-28 16:34:42 -0700

    Update tmux.rb

--- recipes/tmux.rb
+++ recipes/tmux.rb
@@ -1,4 +1,3 @@
 # Copy of pivotal_workstation::tmux minus installing there custom tmux.conf
 include_recipe "pivotal_workstation::homebrew"
-include_recipe "pivotal_workstation::git"
-brew_install "tmux"
+brew "tmux"
