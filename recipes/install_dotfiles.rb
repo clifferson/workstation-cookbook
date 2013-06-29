a09a2051f764c643685a04cebb17b2094496a611
Cliff Erson <cerson@me.com>
2013-06-29 10:38:30 -0700

    move dotfiles to git resource clone

--- recipes/install_dotfiles.rb
+++ recipes/install_dotfiles.rb
@@ -1,5 +1,9 @@
-execute "install_dotfiles" do
-  command "curl -L https://raw.github.com/clifferson/dotfiles/master/install.sh | sh"
-  creates "~/.files"
-  action :run
+#execute "install_dotfiles" do
+#  command "curl -L https://raw.github.com/clifferson/dotfiles/master/install.sh | sh"
+#  creates "~/.files"
+#  action :run
+#end
+
+git "/Users/#{node['current_user']}/.files" do
+  repository "https://github.com/clifferson/dotfiles.git"
 end
