1a4adb5b03605b7f35399a70863c652055eb528f
Cliff Erson <cerson@me.com>
2013-06-29 22:33:02 -0700

    add install_dotvim recipe

---
+++ recipes/install_dotvim.rb
@@ -0,0 +1,15 @@
+git "/Users/#{node['current_user']}/.vim" do
+  repository "https://github.com/clifferson/dotvim.git"
+  revision "master"
+  user node['current_user']
+end
+
+link "/Users/#{node['current_user']}/.vimrc" do
+  to "/Users/#{node['current_user']}/.vim/vimrc"
+  owner node['current_user']
+end
+
+execute "update_git_submodules" do
+  command "git submodule update --init"
+  cwd "/Users/#{node['current_user']}/.vim"
+end
