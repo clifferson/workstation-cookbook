5f8c2093826c7d4d33c07d9104094fcbe45cd7e7
Cliff Erson <cerson@me.com>
2013-09-10 12:02:55 -0700

    update alfred recipe

--- recipes/alfred.rb
+++ recipes/alfred.rb
@@ -7,3 +7,9 @@ alfred2_app_data_dir = "/Users/#{node['current_user']}/Dropbox/Hold/app_data/Alf
 link alfred2_prefs_location_dir do
   to alfred2_app_data_dir
 end
+
+execute 'brew cask alfred link' do
+  command 'brew cask alfred link'
+  user node['current_user']
+  not_if { system("brew cask alfred status | grep 'happily linked' > /dev/null 2>&1") }
+end
