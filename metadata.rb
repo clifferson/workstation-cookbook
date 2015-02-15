763161ff83ef130caa8cc62f0d9f05486f320811
Cliff Erson <cerson@me.com>
2015-02-14 16:04:50 -0800

    remove pivotal cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,3 +1,4 @@
+name             "workstation"
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
@@ -6,6 +7,5 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 depends "dmg"
-depends "pivotal_workstation"
 depends "homebrew"
 depends "python"
