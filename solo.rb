684f8876bb4ba0dd8def1697caf607357b1138a1
Cliff Erson <cerson@me.com>
2015-04-02 05:24:53 -0700

    solo.rb for testing

---
+++ solo.rb
@@ -0,0 +1,4 @@
+#current_dir = File.dirname(__FILE__)
+#cookbook_path "#{current_dir}/cookbooks"
+cookbook_path [ "/Volumes/VMware Shared Folders/workstation-cookbook/cookbooks" ]
+file_cache_path "/tmp/chef"
