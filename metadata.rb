be3f8f003bb4d5a8d27b628b52428ccbb062b62c
Cliff Erson <cerson@me.com>
2015-11-20 07:43:15 -0800

    remove cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,4 @@ maintainer_email "YOUR_EMAIL"
 license          "All rights reserved"
 description      "Installs/Configures workstation"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-
-depends "dmg"
-depends "homebrew"
-depends "python"
+version          "0.2.0"
