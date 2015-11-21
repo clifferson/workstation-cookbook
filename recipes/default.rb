4ad2b4fd1130b0757157c0a947a9b66d99947662
Cliff Erson <cerson@me.com>
2015-11-21 00:58:47 -0800

    wait on alfred, remove linked things

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "workstation::sizeup"
+#include_recipe "workstation::alfred"
 include_recipe "workstation::install_dotfiles"
 include_recipe "workstation::install_dotvim"
 include_recipe "workstation::link_ssh"
+include_recipe "workstation::sizeup"
