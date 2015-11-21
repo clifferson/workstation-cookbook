e4323a88471d673725a386dc24803a02faf19a64
Cliff Erson <cerson@me.com>
2015-11-21 01:02:10 -0800

    updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "workstation"
 maintainer       "YOUR_COMPANY_NAME"
 maintainer_email "YOUR_EMAIL"
-license          "All rights reserved"
-description      "Installs/Configures workstation"
+license          "Go ahead"
+description      "Installs/Configures os x workstation fmbm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.0"
