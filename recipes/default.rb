6c6b20bf4e70294180a06b5e4eaf29f27672377f
Cliff Erson <cerson@me.com>
2015-11-25 15:50:11 -0800

    actually run the link_plists recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,4 +6,5 @@
 include_recipe "workstation::install_dotfiles"
 include_recipe "workstation::install_dotvim"
 include_recipe "workstation::link_ssh"
+include_recipe "workstation::link_plists"
 include_recipe "workstation::sizeup"
