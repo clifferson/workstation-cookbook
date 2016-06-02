db4d531be61591b2a78375f6da239908649324ef
Cliff Erson <cerson@me.com>
2016-06-02 16:43:58 -0700

    remove dropbox

--- install.sh
+++ install.sh
@@ -51,16 +51,6 @@ ${BREW} cleanup
 echo "installing cask..."
 ${BREW} install caskroom/cask/brew-cask
 
-# Install Dropbox first outa the loop because a lot of things live there
-if ${BREW} cask list dropbox
-then
-  ${BREW} cask install dropbox
-else
-  ${BREW} cask install dropbox
-  open ~/Applications/Dropbox.app
-  read -p "Press [Enter] after you have logged into Dropbox"
-fi 
-
 # Brew cask install some apps
 apps=(
  alfred
