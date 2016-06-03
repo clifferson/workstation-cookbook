208a5fc80da7ffb8a6a9d382de6ee3abdbfd449a
Cliff Erson <cerson@me.com>
2016-06-02 18:31:44 -0700

    kidding, keep the dropbox_dir attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,7 @@ default['dirs']['library']             = "#{node['dirs']['homedir']}/Library"
 default['dirs']['icloud_dir']          = "#{node['dirs']['library']}/Mobile\ Documents/com~apple~CloudDocs/Hold/app_data"
 default['dirs']['library_prefs']       = "#{node['dirs']['library']}/Preferences"
 default['dirs']['library_app_support'] = "#{node['dirs']['library']}/Application\ Support"
+
+# Remove this at some point
+default['dirs']['dropbox_dir']            = "#{node['dirs']['homedir']}/Dropbox/Hold/app_data"
+
