a852da2814592372992a2769c7491998e8936b67
Cliff Erson <cerson@me.com>
2016-06-15 17:56:24 -0700

    link ssh from icloud not drop box

--- recipes/link_ssh.rb
+++ recipes/link_ssh.rb
@@ -1,5 +1,5 @@
 ssh_dir = "#{node.dirs.homedir}/.ssh"
-ssh_dbox_dir = "#{node.dirs.homedir}/Dropbox/Hold/ssh_config"
+ssh_dbox_dir = "#{node.dirs.icloud_hold_dir}/ssh_config"
 
 link ssh_dir do
   to ssh_dbox_dir
