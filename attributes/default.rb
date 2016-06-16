aa641c3c23ba66eba152ac4798193a5b05d54108
Cliff Erson <cerson@me.com>
2016-06-15 21:55:16 -0700

    delete /hold/hold :|

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,7 +5,7 @@ default['dirs']['library']             = "#{node['dirs']['homedir']}/Library"
 
 default['dirs']['icloud_dir']          = "#{node['dirs']['library']}/Mobile\ Documents/com~apple~CloudDocs"
 default['dirs']['icloud_hold_dir']     = "#{node['dirs']['icloud_dir']}/Hold"
-default['dirs']['icloud_app_data_dir'] = "#{node['dirs']['icloud_hold_dir']}/Hold/app_data"
+default['dirs']['icloud_app_data_dir'] = "#{node['dirs']['icloud_hold_dir']}/app_data"
 
 default['dirs']['library_prefs']       = "#{node['dirs']['library']}/Preferences"
 default['dirs']['library_app_support'] = "#{node['dirs']['library']}/Application\ Support"
